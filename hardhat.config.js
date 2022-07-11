dd207a8d9a472b43a2ce033ca0c0d0dbaf55818a
Pablo Acebedo <pablofelipe@me.com>
2022-07-11 11:34:55 -0500

    Ready to test in real world

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   solidity: "0.8.4",
@@ -8,10 +10,14 @@ module.exports = {
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
   },
-  defaultNetwork: "ganache",
+  // defaultNetwork: "ganache",
   networks: {
     ganache: {
       url: "http://127.0.0.1:7545"
+    },
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com/v1/a7cd698577c71979e16092f63beeb5fa02e3ace0",
+      accounts: [privateKey]
     },
     hardhat: {
     },
